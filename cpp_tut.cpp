f55865f0bc7626dd89cac0359279ed2c37299de3
Rahul Jangra <jangraboy.nature@gmail.com>
2025-06-29 23:25:06 +0530

    I have done a bit more questions

--- cpp_tut.cpp
+++ cpp_tut.cpp
@@ -249,13 +249,41 @@ using namespace std;
 //     }
 // }
 
-int main(){
-    int n=4;
-    for(int i=0; i<n;i++){
-        int count = 1;
-        for(int j=0; j<i+1;j++){
-            cout << count++;
-        }
-        cout << endl;
-    }
-}
+// int main(){
+//     int n=4;
+//     for(int i=0; i<n;i++){
+//         int count = 1;
+//         for(int j=0; j<i+1;j++){
+//             cout << count++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// int main(){
+//     int n =4;    
+//     for(int i=1; i<=n; i++){        
+//         for(int j=0; j<i; j++){
+//             cout << i;           
+//         }    
+//         cout<<endl;
+//     } 
+// }
+
+
+
+// int main() {
+//     int n = 4;
+//     char ch = 'A';
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= i; j++) {
+//             cout << ch;
+//         }
+//         cout << endl;
+//         ch++;
+//     }
+//     return 0;
+// }
+
+
