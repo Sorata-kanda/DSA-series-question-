80682525caf35efad6f204c99d71e8b1490d6357
Rahul Jangra <jangraboy.nature@gmail.com>
2025-06-30 01:01:52 +0530

    inverted traingle patterns added

--- cpp_tut.cpp
+++ cpp_tut.cpp
@@ -315,3 +315,104 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+
+// int main(){
+//     int n = 4;
+//     int count =1;
+//     for(int i=0;i<n;i++){
+//         for(int j=0; j<i+1; j++){
+//             cout<<count++<< " ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// int main(){
+//     int n=4;
+//     char ch='A';
+//     for(int i=0; i<n;i++){
+//         for(int j=0;j<i+1;j++){
+//             cout<<ch++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//Reverse alphabets order
+// int main(){
+//     int n=4;
+//     char ch='Z';
+//     for(int i=0; i<n;i++){
+//         for(int j = i+1;j>0;j--){
+//             cout<<ch--;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+// ----------------------------------------------------INVERTED TRIANGLES--------------------------------------------------------
+
+
+
+// int main(){
+//     int n=4;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<i;j++){
+//             cout<<" ";
+//         }
+    
+//         for(int k=0; k<n-i; k++){
+//             cout<<i+1;
+//         }
+//         cout<<endl;
+      
+//     }
+//     return 0;
+// }
+
+
+//char version
+
+
+// int main(){
+//     int n=4;
+//     char ch= 'A';
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<i;j++){
+//             cout<<" ";
+//         }
+    
+//         for(int k=0; k<n-i; k++){
+//             cout<<ch;
+//         }
+//         cout<<endl;
+//         ch++;
+//     }
+// }
+
+
+//inversted piramid
+
+
+int main(){
+    int n=4;
+    for(int i=1; i<=n; i++){
+        
+    
+        for(int k=0; k<n-i; k++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i;j++){
+            cout<<j;
+        }
+        for(int j=1; j<i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }    
+}
