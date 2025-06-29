c776bb4bb4d6e61e56cb559b255e8dcf954de9ab
Rahul Jangra <jangraboy.nature@gmail.com>
2025-06-29 23:54:08 +0530

    mroe questions

--- cpp_tut.cpp
+++ cpp_tut.cpp
@@ -287,3 +287,31 @@ using namespace std;
 // }
 
 
+
+// int main(){
+//     int n=4;
+//     for(int i=0; i<n;i++){
+
+//         for (int j=1; j<=i+1;j++){
+//             cout<<j;
+//         }
+
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+//reverse triangle pattern
+
+// int main(){
+//     int n=4;
+
+//     for(int i=0; i<n; i++){
+//         for(int j = i+1; j>0;j--){
+//             cout<< j;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
