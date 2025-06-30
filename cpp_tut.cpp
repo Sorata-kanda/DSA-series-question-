ba342c321520f7af05149e435bd12658efeb9126
Rahul Jangra <jangraboy.nature@gmail.com>
2025-06-30 22:12:53 +0530

    Some wrong changes kindly ignore!

--- cpp_tut.cpp
+++ cpp_tut.cpp
@@ -455,3 +455,29 @@ using namespace std;
 
 
 // }
+
+
+
+//teacher's done!
+
+int main(){
+    int n=4;
+    //top
+    for(int i=0; i<n; i++) {
+    //spaces
+        for(int j=0; j<n-i-1; j++) {
+        cout << " ";
+
+        }
+
+        cout << "*";
+
+        if(i != 0) {
+        //spaces
+        for(int j=0; j<2*i-1; j++) {
+        cout << " ";
+
+        cout << "*";
+
+    }cout << endl;
+}
