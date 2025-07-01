cec22deb150ba730a2c1017d61c294af9978ba14
Rahul Jangra <jangraboy.nature@gmail.com>
2025-07-02 03:30:32 +0530

    fix the hollow diamond question!

--- cpp_tut.cpp
+++ cpp_tut.cpp
@@ -449,7 +449,12 @@ using namespace std;
 //         for(int j=n-i;j>n-i-1;j--){
 //             cout<<"*";
 //         }
-//         for(int j=n-i+1;j)
+//         for(int j=n-i;j<2*i-3;j++){
+//             cout<<" ";
+//         }
+//         for(int j=n-i;j<i+4;j++){
+//             cout<<"*";
+//         }
 //         cout<<endl;
 //     }
 
@@ -457,27 +462,47 @@ using namespace std;
 // }
 
 
-
-//teacher's done!
-
 int main(){
     int n=4;
-    //top
-    for(int i=0; i<n; i++) {
-    //spaces
-        for(int j=0; j<n-i-1; j++) {
-        cout << " ";
+    // top
+    for(int i=0; i<n; i++){
+        // spaces
+        for(int j=0;j<n-i-1;j++){
+            cout<< " ";
+        }
+        cout<<"*";
 
+
+        // spaces
+        if(i!=0){
+            for(int j=0;j<2*i-1;j++){
+                cout<< " ";
+            }
+            cout<< "*";
         }
 
-        cout << "*";
 
-        if(i != 0) {
-        //spaces
-        for(int j=0; j<2*i-1; j++) {
-        cout << " ";
 
-        cout << "*";
-
-    }cout << endl;
+        cout<<endl;
+    }
+    //bottom
+    for(int i=0; i<n-1; i++){
+        // spaces
+        for(int j=0 ;j<i+1;j++){
+            cout<<" ";
+        }
+        cout<<"*";
+        // spaces
+        if(i!=n-2){
+            for(int j=0; j<2*(n-i)-5;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }   
+        
+        cout<< endl;
+    }
+    return 0;
 }
+
+
