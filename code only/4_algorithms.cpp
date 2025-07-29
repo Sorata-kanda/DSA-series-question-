b733c55bc60294f02bb9890094269733d5e19cea
sorata-kanda <jangraboy.nature@gmail.com>
2025-07-29 23:55:05 +0530

    Day - 18 completed //A hard logic to understand [medium question]

--- code only/4_algorithms.cpp
+++ code only/4_algorithms.cpp
@@ -58,26 +58,62 @@ using namespace std;
 
 
 // =================using recursion====================
-int bb(vector<int> arr,int target,int start, int end){
+// int bb(vector<int> arr,int target,int start, int end){
     
-    if(start <=end){
-        int mid = start + (end - start)/2;
-        if(target<arr[mid]){
-            return bb(arr,target,start,mid-1);
-        }
-        else if(target>arr[mid]){
-            return bb(arr,target,mid+1,end);
-        }
-        else{
-            return mid;
-        }
-    }
-    return -1; 
-}
+//     if(start <=end){
+//         int mid = start + (end - start)/2;
+//         if(target<arr[mid]){
+//             return bb(arr,target,start,mid-1);
+//         }
+//         else if(target>arr[mid]){
+//             return bb(arr,target,mid+1,end);
+//         }
+//         else{
+//             return mid;
+//         }
+//     }
+//     return -1; 
+// }
+
+// int main(){
+//     vector<int> arr = {-1,0,3,5,9,12};
+//     int target =  12;
+//     int start = 0, end = arr.size()-1;
+//     cout<<bb(arr,target,start,end);
+// }
+
+
+// ---------------------------------------------Search in Rotated array-----------------------------------------------------
+
 
 int main(){
-    vector<int> arr = {-1,0,3,5,9,12};
-    int target =  12;
-    int start = 0, end = arr.size()-1;
-    cout<<bb(arr,target,start,end);
+    vector<int> arr = {3,4,5,6,7,0,1,2};
+    int tar = 0, start = 0, end = arr.size() - 1, mid;
+    while(start <= end){
+        mid = start + (end - start) / 2;
+        if(arr[mid] == tar){
+            cout << mid;
+            break;
+        }
+        // Check if the left half is sorted
+        if(arr[start] <= arr[mid]){
+            if(arr[start] <= tar && tar < arr[mid]){ // Logic error: should compare with arr[mid]
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        // Otherwise, the right half must be sorted
+        else{
+            if(arr[mid] <= tar && tar <= arr[end]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+    }
+    return 0;
 }
+
