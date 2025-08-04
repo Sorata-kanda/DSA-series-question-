48489d2f56c77a93c30e1e2e8dcd0b44877d5d9b
sorata-kanda <jangraboy.nature@gmail.com>
2025-08-04 23:34:01 +0530

    Day - 21 completed // Hard question done! [need to do myself]

--- code only/4_algorithms.cpp
+++ code only/4_algorithms.cpp
@@ -1,3 +1,5 @@
+//[not uploaded] - these problem are not found but solved here
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -298,23 +300,83 @@ using namespace std;
 // }
 
 //----------------------------------palindrome number-------------------------------------------------------
-int main(){
-    int x = -121;
-    int temp = x;
-    int y=0;
-    if(x<0){
-        cout<<"false";
+// int main(){
+//     int x = -121;
+//     int temp = x;
+//     int y=0;
+//     if(x<0){
+//         cout<<"false";
+//     }
+//     else{
+//         while(x>0){
+//             y = y*10 + x%10;
+//             x/=10;
+//             // cout<<y<<" ";
+//         }
+//         if(temp==y){
+//             cout<<"true";
+//         }else{
+//             cout<<"false";
+//         }
+//     }
+// }
+
+
+//----------------------------------Book allocation [Hard] [not uploaded]------------------------------------------------
+
+bool isValid(vector<int> arr, int n, int m, int mid){
+    int student = 1, pages =0;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>mid){
+            return false;
+        }
+
+        if(pages + arr[i]<=mid){
+            pages += arr[i];
+        }else{
+            student++;
+            pages = arr[i];
+        }
+    }
+    if(student == m){
+        return true;
     }
     else{
-        while(x>0){
-            y = y*10 + x%10;
-            x/=10;
-            // cout<<y<<" ";
+        return false;
+    }
+}
+
+
+
+int allocateBooks(vector<int> arr, int n, int m){
+    if(m>n){
+        return -1;
+    }
+    int summ =0;
+    for(int value: arr){
+        summ += value; 
+    }
+    int start =0 ,end = summ,ans=-1;
+    while(start<=end){
+        int mid = start + (end -start)/2 ;
+        if(isValid(arr, n, m, mid)){
+            ans = mid;
+            end = mid-1;
         }
-        if(temp==y){
-            cout<<"true";
-        }else{
-            cout<<"false";
+        else{
+            start = mid+1;
         }
     }
+    return ans;
+}
+
+int main(){
+    // vector<int> arr = {2, 1, 3, 4};
+    vector<int> arr = {15,17,20};
+    
+    int n=4 ,m=2;
+
+    cout<< allocateBooks(arr,n,m)<<endl;
+    return 0;
 }
